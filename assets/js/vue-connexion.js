d7aaf3d07235e74c2760f43948eb73a4621721d5
Colin-Joubert <colin.joubert@edu.ece.fr>
2022-06-07 20:07:32 +0200

    CODE_REVIEW_FIX register.html start register_page.php

---
+++ assets/js/vue-connexion.js
@@ -0,0 +1,29 @@
+Vue.component('register', {
+    template: `
+    
+        <div class="container">
+            <h1>Creation de compte</h1>
+            <p>Veuillez remplir ce formulaire pour créer un compte.</p>
+            <hr>
+
+            <label for="email"><b>E-mail</b></label>
+            <input type="text" placeholder="exemple@gmail.com" name="email" id="email" required>
+
+            <label for="psw"><b>Mot de Passe</b></label>
+            <input type="password" placeholder="MotDePasse" name="psw" id="psw" required>
+
+            <label for="psw-repeat"><b>Repetez le mot de passe</b></label>
+            <input type="password" placeholder="MotDePasse" name="psw-repeat" id="psw-repeat" required>
+            <hr>
+
+            <p>En créant ce compte, vous adhérez à nos <a href="#">Conditions d'Utilisation</a></p>
+            <button type="submit" class="registerbtn">Creation</button>
+            <br>
+            <p>Vous avez déjà un compte chez nous? <a href="login.html">Login</a>.</p>
+        </div>
+    `
+})
+
+var register = new Vue({
+    el: "#register-form"
+})

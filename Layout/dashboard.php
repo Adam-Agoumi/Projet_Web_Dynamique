7ff1e83db13be5e9d1b550ff360ed1f53569b958
guillaume <guillaume.jean@edu.ece.fr>
2022-06-08 22:29:11 +0200

    connexion

--- Layout/dashboard.php
+++ Layout/dashboard.php
@@ -1,10 +1,19 @@
 <?php
-    $database = "Books";
-    $db_handle = mysqli_connect('webdynamiquegroupe3.database.windows.net', 'Guillaume', 'RootRoot3') or die('Erreur de connexion');
+
+$host = 'webdynamiquegroupe3.database.windows.net';
+$username = 'Guillaume';
+$password = 'RootRoot3';
+$database = "Books";
+
+
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+    $db_handle = mysqli_real_connect(($conn, 'webdynamiquegroupe3.database.windows.net', 'Guillaume', 'RootRoot3','Books',1436,NULL,MYSQLI_CLIENT_SSL) or die('Erreur de connexion');
     $db_found = mysqli_select_db($db_handle, $database) or die('Base de données inexistante');
 
     if($db_found) {
-        $sql = "SELECT * FROM Book WHERE BookApproved = 1";
+        $sql = "SELECT * FROM Book WHERE BookApprroved = 1";
         $result = mysqli_query($db_handle, $sql) or die('Erreur SQL <br>' . $sql . '<br>' . mysqli_error($db_handle));
         if(mysqli_num_rows($result) == 0){
             echo "Il n'y a pas de livres validés";

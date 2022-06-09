764858a6a1263c4d158a9d3e663e3c39455b650e
Colin-Joubert <colin.joubert@edu.ece.fr>
2022-06-09 16:18:07 +0200

    dashboard CODE_REVIEW_FIX

--- Layout/dashboard.php
+++ Layout/dashboard.php
@@ -15,7 +15,11 @@
         return $connection;
     }
 
-    $sqlTitle = "SELECT Title FROM Book WHERE approbation = 1";
+    $sqlTitle = "SELECT Book.Title, BookAutor.AutorName, Autors.LastName 
+                 FROM (Book 
+                     INNER JOIN (BookAutor INNER JOIN Autors ON BookAutor.AutorName = Autors.FirstName) 
+                     ON Book.Title = BookAutor.BookTitle)
+                 WHERE approbation = 1";
     $resultTitle = $connection->query($sqlTitle);
 
     if($resultTitle->rowCount() == 0){
@@ -25,25 +29,23 @@
 
         //on commence un tableau html
         echo "\n<table>\n<tr>\n" .
-             "\n\t<th>Titre</th>" /*.
-             "\n\t<th>Date de publication</th>" .
-             "\n\t<th>Editeur</th>" .
-             "\n\t<th>Genre</th>" .
-             "\n\t<th>Collection</th>" .
-             "\n</tr>"*/;
+             "\n\t<th>Titre</th>" .
+             "\n\t<th>Auteur</th>" .
+             "\n\t<th> </th>";
 
         while($row = $resultTitle->fetch(PDO::FETCH_ASSOC)){ //jusqu'à plus de row dans le result
             echo "\n<tr>"; //on commence un row du tableau
 
             //on sort chacun des attributs comme table data
-            foreach ($row as $data)
-                echo "\n\t<td> $data </td>";
+            foreach ($row as $dataTitle) {
+                echo "\n\t<td> $dataTitle</td>";
+            }
 
             //on finit le row
             echo "\n</tr>";
-            }
-            //finir le tableau
-            echo "\n</table>\n";
         }
+        //finir le tableau
+        echo "\n</table>\n";
+    }
     $connection=null;
 ?>

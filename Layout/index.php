7a453da7924a4847f19f961487f33631ec85d63b
guillaume <guillaume.jean@edu.ece.fr>
2022-06-09 17:05:04 +0200

    debugging time

--- Layout/index.php
+++ Layout/index.php
@@ -16,62 +16,6 @@
             <login></login>
         </form>
     </div>
+
 </body>
 </html>
-<?php
-
-$username = isset($_POST["username"]) ? $_POST["username"] : "";
-$password = isset($_POST["password"]) ? $_POST["password"] : "";
-$connection=null;
-require './deconnexion.php';
-
-    if(isset($_POST["LoginButton"])){
-
-        if(session_status() == PHP_SESSION_NONE){
-
-
-
-            if(!empty($username)&& !empty($password)){
-                require './script/bdd_users_connect.php';
-                $sql = "SELECT * FROM user INNER JOIN userrole ON user = userrole.RoleID WHERE( user.Username = '" . $username . "' AND user.password = '" . $password . "')";
-                $result = $connection->query($sql);
-
-                if(!$result){
-                    $connection = null;
-                    exit();
-                }
-
-            if ($result->rowCount() > 0){
-                foreach ($result as $row){
-                    $isAllowed = true;
-                    session_start();
-                    $SESSION["role"]=$row["Roles"];
-                    $SESSION["username"]=$username;
-                    $SESSION["Approved"]=$row["Approved"];
-                    $SESSION["LastName"]=["nom"];
-                    $SESSION["FirstName"]=["prénom"];
-
-                    $password = null;
-                    $username = null;
-                }
-            }
-            if (!$isAllowed){
-                $error = "le mot de passe est incorrect";
-                echo "<div style='color: #DA291C; background-color: lightgray; border: 3px solid firebrick;'> Le mot de passe est incorrect  </div><br><br>";
-                session_destroy();
-            }
-        } else {
-                $error = "l'utilisateur n'est pas dans la base de donnée";
-                echo "<div style='color: #DA291C; background-color: lightgray; border: 3px solid firebrick;'> l'utilisateur n'est pas dans la base de donnée  </div><br><br>";
-                session_destroy();
-            }
-    }else{
-            $error = "il manque des informations";
-            echo "<div style='color: #DA291C; background-color: lightgray; border: 3px solid firebrick;'> il manque des informations  </div><br><br>";
-            session_destroy();
-
-        }
-}
-
-$connection=null;
-?>
--- Layout/login_page.php
+++ Layout/login_page.php
@@ -1,28 +1,61 @@
 <?php
 
-    $database = "Users";
-    $servername ="localhost";
-    $usernameDB = "root";
-    $passwordDB = "";
-    $charset = "utf8mb4";
+$email = isset($_POST["email"]) ? : "";
+$password = isset($_POST["psw"]) ?  : "";
+$isAllowed = false;
+$connection=null;
+//require './deconnexion.php';
 
-    try {
-        $datasource = "mysql:host=$servername; dbname=$database; charset=$charset";
-        $connection = new PDO($datasource, $usernameDB, $passwordDB);
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        print("Connection reussie.");
+if(isset($_POST["LoginButton"])){
+
+    if(session_status() == PHP_SESSION_NONE){
+
+
+
+        if(!empty($email)&& !empty($password)){
+            require '../script/bdd_users_connect.php';
+            $sql = "SELECT * FROM user 
+                    INNER JOIN userrole 
+                    ON User_id = userrole.UserID 
+                    WHERE( user.Email = '" . $email . "' AND user.password = '" . $password . "')";
+            $result = $connection->query($sql);
+
+            if(!$result){
+                $connection = null;
+                exit();
+            }
+
+            if ($result->rowCount() > 0){
+                foreach ($result as $row){
+                    $isAllowed = true;
+                    session_start();
+                    $SESSION["role"]=$row["Roles"];
+                    $SESSION["username"]=$username;
+                    $SESSION["Approved"]=$row["Approved"];
+                    $SESSION["LastName"]=["nom"];
+                    $SESSION["FirstName"]=["prénom"];
+
+                    $password = null;
+                    $username = null;
+                }
+            }
+            if (!$isAllowed){
+                $error = "le mot de passe est incorrect";
+                echo "<div style='color: #DA291C; background-color: lightgray; border: 3px solid firebrick;'> Le mot de passe est incorrect  </div><br><br>";
+                session_destroy();
+            }
+        } else {
+            $error = "l'utilisateur n'est pas dans la base de donnée";
+            echo "<div style='color: #DA291C; background-color: lightgray; border: 3px solid firebrick;'> l'utilisateur n'est pas dans la base de donnée  </div><br><br>";
+            session_destroy();
+        }
+    }else{
+        $error = "il manque des informations";
+        echo "<div style='color: #DA291C; background-color: lightgray; border: 3px solid firebrick;'> il manque des informations  </div><br><br>";
+        session_destroy();
+
     }
-    catch (PDOException $e) {
-        print("Error connecting to SQL Server.");
-        die(print_r($e));
-    }
+}
 
-    $email = $_POST["email"] ?? "";
-    $psw = $_POST["psw"] ?? "";
-
-    if($_POST["registerbtn"]){
-        $sql = "SELECT * FROM [User] WHERE Email=? AND Password=?";
-        if()
-    }
-
+$connection=null;
 ?>

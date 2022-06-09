3f8b25c3e527ce1e04fdbfe7e04c62cd8397eb91
Colin-Joubert <colin.joubert@edu.ece.fr>
2022-06-09 11:31:21 +0200

    dashboard et searchForm CODE_REVIEW_FIX

--- Layout/dashboard.php
+++ Layout/dashboard.php
@@ -1,31 +1,86 @@
 <?php
 
-$host = 'webdynamiquegroupe3.database.windows.net';
-$username = 'Guillaume';
-$password = 'RootRoot3';
-$database = "Books";
+    /*$host = 'webdynamiquegroupe3.database.windows.net';
+    $username = 'Guillaume';
+    $password = 'RootRoot3';
+    $database = "Books";*/
+    /*$servername ="tcp:webdynamiquegroupe3.database.windows.net,1433";
+    $connectionOptions = array(
+        "Database" => "Books",
+        "UID" => "Guillaume",
+        "PWD" => "RootRoot3"
+    );
+    //$conn = mysqli_init();
+    $conn = sqlsrv_connect($servername, $connectionOptions);
 
+    if($conn === false){
+        die(print_r(sqlsrv_errors(), true));
+    }*/
 
-$conn = mysqli_init();
+    //mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+    //$db_handle = mysqli_real_connect($conn, 'webdynamiquegroupe3.database.windows.net', 'Guillaume', 'RootRoot3','Books',1433,NULL,MYSQLI_CLIENT_SSL) or die('Erreur de connexion');
+    /*$db_handle = mysqli_connect('localhost', 'root', '');
+    $db_found = mysqli_select_db($db_handle, $database) or die('Base de données inexistante');*/
 
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-    $db_handle = mysqli_real_connect(($conn, 'webdynamiquegroupe3.database.windows.net', 'Guillaume', 'RootRoot3','Books',1436,NULL,MYSQLI_CLIENT_SSL) or die('Erreur de connexion');
-    $db_found = mysqli_select_db($db_handle, $database) or die('Base de données inexistante');
+    $database = "books";
+    $servername ="localhost";
+    $usernameDB = "root";
+    $passwordDB = "";
+    $charset = "utf8mb4";
 
-    if($db_found) {
-        $sql = "SELECT * FROM Book WHERE BookApprroved = 1";
-        $result = mysqli_query($db_handle, $sql) or die('Erreur SQL <br>' . $sql . '<br>' . mysqli_error($db_handle));
-        if(mysqli_num_rows($result) == 0){
+    try{
+        $datasource = "mysql:host=$servername; dbname=$database; charset=$charset";
+        $connection = new PDO($datasource, $usernameDB, $passwordDB);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $e){
+        echo "connection failed :" . $e->getMessage();
+        return $connection;
+    }
+
+    //if($db_found) {
+        $sql = "SELECT * FROM Book WHERE approbation = 1";
+        //$result = mysqli_query($datasource, $sql);//or die('Erreur SQL <br>' . $sql . '<br>' . mysqli_error($datasource));
+        $result = $connection->query($sql);
+        //if(mysqli_num_rows($result) == 0){
+        if($result->rowCount() == 0){
             echo "Il n'y a pas de livres validés";
         }else{
-            while($data = mysqli_fetch_assoc($result)){
-                echo '<b>Titre: ' . $data['Title'] . '</b>';
+            //while($data = mysqli_fetch_assoc($result)){
+            while($data = $connection->fetch(PDO::FETCH_ASSOC)){
+
+                echo "<h1>Livres correspondants à la recherche</h1>\n";
+                //on commence un tableau html
+                echo "\n<table>\n<tr>\n" .
+                    "\n\t<th>ID Livre</th>" .
+                    "\n\t<th>Titre</th>" .
+                    "\n\t<th>Auteur</th>" .
+                    "\n\t<th>Editeur</th>" .
+                    "\n\t<th>Année</th>" .
+                    "\n</tr>";
+
+                //jusqu'à plus de row dans le result
+                while($row = @ mysqli_fetch_row($result)){
+                    //on commence un row du tableau
+                    echo "\n<tr>";
+
+                    //on sort chacun des attributs comme table data
+                    foreach ($row as $data)
+                        echo "\n\t<td> $data </td>";
+
+                    //on finit le row
+                    echo "\n</tr>";
+                }
+                //finir le tableau
+                echo "\n</table>\n";
+
+                /*echo '<b>Titre: ' . $data['Title'] . '</b>';
                 echo '<i>Auteur: ' . $data['Author'] . '</i>';
-                echo 'Editeur: '.$data['Editor'];
+                echo 'Editeur: '.$data['Editor'];*/
             }
         }
-    }else{
+    /*}else{
         echo "Database not found";
-    }
-    mysqli_close($db_handle);
+    }*/
+    //mysqli_close($datasource);
+    $connection=null;
 ?>
--- Layout/searchForm.php
+++ Layout/searchForm.php
@@ -1,30 +1,85 @@
+<!DOCTYPE HTML PUBLIC
+    "-//W3C//DTD HTML 4.0 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Search Form</title>
+</head>
+<body>
 <?php
-    $database = "Books";
-    $db_handle = mysqli_connect('localhost', 'root', '') or die('Erreur de connexion');
-    $db_found = mysqli_select_db($db_handle, $database) or die('Base de données inexistante');
+    $database = "books";
+    $servername ="localhost";
+    $usernameDB = "root";
+    $passwordDB = "";
+    $charset = "utf8mb4";
 
-    $titre = isset($_POST["titre"])? $_POST["titre"] : "";
-    $auteur = isset($_POST["auteur"])? $_POST["auteur"] : "";
-    $annee = isset($_POST["annee"])? $_POST["annee"] : "";
-    $editeur = isset($_POST["editeur"])? $_POST["editeur"] : "";
+    $titre = $_POST["titre"] ?? "";
+    $auteur = $_POST["auteur"] ?? "";
+    $annee = $_POST["annee"] ?? "";
+    $editeur = $_POST["editeur"] ?? "";
 
-    if($_POST["submitbtn"]){
-        if($db_found) {
-            $sql = "SELECT * FROM Book";
-            /*$result = mysqli_query($db_handle, $sql) or die('Erreur SQL <br>' . $sql . '<br>' . mysqli_error($db_handle));
-            if(mysqli_num_rows($result) == 0){
-                echo "Il n'y a pas de livres validés";
-            }else{
-                while($data = mysqli_fetch_assoc($result)){
-                    echo '<b>Titre: ' . $data['Title'] . '</b>';
-                    echo '<i>Auteur: ' . $data['Author'] . '</i>';
-                    echo 'Editeur: '.$data['Editor'];
-                }
-            }*/
-        }else{
-            echo "Database not found";
-        }
+    try{
+        $datasource = "mysql:host=$servername; dbname=$database; charset=$charset";
+        $connection = new PDO($datasource, $usernameDB, $passwordDB);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $e){
+        echo "connection failed :" . $e->getMessage();
+        return $connection;
     }
 
-    mysqli_close($db_handle);
+    if($_POST["submitbtn"]){
+        //if($db_found){
+            $sql = "SELECT * FROM Book";
+            if($titre != ""){
+                $sql .= " WHERE Title LIKE '%$titre%'";
+                if($auteur != ""){
+                    $sql .= " AND Autor LIKE '%$auteur%'";
+                }
+            }
+            $result = mysqli_query($datasource, $sql) or die('Erreur SQL <br>' . $sql . '<br>' . mysqli_error($datasource));
+            if(mysqli_num_rows($result) == 0){
+                echo "Il n'y a pas de livres dans cette recherche";
+            }else{
+                while($data = mysqli_fetch_assoc($result)){
+                    /*echo '<b>Titre: ' . $data['Title'] . '</b>';
+                    echo '<i>Auteur: ' . $data['Author'] . '</i>';
+                    echo 'Editeur: ' . $data['Editor'];
+                    echo 'Année: ' . $data['Year'];
+                    echo 'ID: ' . $data['ID'];*/
+                    echo "<h1>Livres correspondants à la recherche</h1>\n";
+
+                    //on commence un tableau html
+                    echo "\n<table>\n<tr>\n" .
+                         "\n\t<th>ID Livre</th>" .
+                         "\n\t<th>Titre</th>" .
+                         "\n\t<th>Auteur</th>" .
+                         "\n\t<th>Editeur</th>" .
+                         "\n\t<th>Année</th>" .
+                         "\n</tr>";
+
+                    //jusqu'à plus de row dans le result
+                    while($row = @ mysqli_fetch_row($result)){
+                        //on commence un row du tableau
+                        echo "\n<tr>";
+
+                        //on sort chacun des attributs comme table data
+                        foreach ($row as $data)
+                            echo "\n\t<td> $data </td>";
+
+                        //on finit le row
+                        echo "\n</tr>";
+                    }
+                    //finir le tableau
+                    echo "\n</table>\n";
+
+                }
+            }
+        /*}else{
+            echo "Database not found";
+        }*/
+    }
+
+    mysqli_close($datasource);
 ?>
+</body>
+</html>

3119e3141b25b8110ce9955ce6f9316699dde879
adamagoumi <adam.agoumi@edu.ece.fr>
2022-06-09 18:03:16 +0200

    Test

---
+++ Layout/page_d_acceuil.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/index.css">
+    <link rel="stylesheet" href="../assets/css/navbar.css">
+    <title>Acceuil</title>
+</head>
+<body>
+<nav class="navbar navbar-custom fixed-top navbar-expand-lg ">
+    <!-- Links -->
+    <ul class="navbar-nav" >
+        <a class="navbar-brand"><img src="../assets/img/logo.png" height=50></a>
+        <li class="nav-item">
+            <a class="nav-link nav-link-custom text-light" href="../Layout/register.php" style="margin-left: 800px">Inscription</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav-link-custom text-light" href="../Layout/index.php">Connection</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav-link-custom text-light" href="#">Visiteur</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav-link-custom text-light" href="../Layout/page_d_acceuil.php">Acceuil_test</a>
+        </li>
+    </ul>
+
+</nav>
+
+<div class="info-container">
+    <div class="title-container">
+        <h1>Bienvenue dans notre bibliothèque numérique</h1>
+    </div>
+    <div class="book-info">
+        <div class="cover-image">
+            <img src="../assets/img/cover.jpeg">
+        </div>
+        <div class="book-text-info">
+            <p><strong> Voici un exemple d'informations sur un livre que vous pourrez retrouver dans notre bibliothèque</strong></p>
+            <p><strong>Titre : </strong>Atomic Habits</p>
+            <p><strong>Auteur : </strong>James Clear</p>
+            <p><strong>Editeur : </strong>Avery</p>
+            <p><strong>Année de parution : </strong>2018</p>
+            <p><strong>ISBN : </strong>B01N5AX61W</p>
+            <p><strong>Nombres de pages : </strong>319</p>
+            <p><strong>Description : </strong> Quels que soient vos objectifs, Atomic Habits vous propose un cadre éprouvé pour vous améliorer - chaque jour. James Clear, l'un des plus grands spécialistes mondiaux de la formation des habitudes, révèle des stratégies pratiques qui vous apprendront exactement comment former de bonnes habitudes, briser les mauvaises et maîtriser les petits comportements qui conduisent à des résultats remarquables.</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
